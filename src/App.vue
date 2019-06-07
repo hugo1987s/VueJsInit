7b595f8ed4448528975a0eb1bd0a1e9caee8e1ca
Schreiber <hugo.schreiber@accenture.com>
2019-06-07 16:24:37 -0300

    se agrega el componente Cars

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="Container text-center">
+    <img alt="Vue logo" src="./assets/logo.png">
+      <automoviles></automoviles>
+  </div>
+</template>
+
+<script>
+
+import Automoviles from './components/Automoviles.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Automoviles
+  }
+}
+</script>
---
+++ src/components/Automoviles.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Automoviles</h1>
+    </div>
+</template>
+    
+<script>
+
+export default {
+    name: 'Cars'    
+}
+
+</script>
+
+
